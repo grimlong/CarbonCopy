90469be73f793a636d59b278f136a1a563959c82
Jason L MIller <65247797+grimlong@users.noreply.github.com>
2021-11-03 17:31:38 -0400

    Added more to comments

--- app/src/main/java/com/example/carboncopy/MainActivity.kt
+++ app/src/main/java/com/example/carboncopy/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    // Initial values for all players
+    // Initial values for all players This is the Starting Health for the MTG Commander Format
     private var p1Score = 40
     private var p2Score = 40
     private var p3Score = 40
@@ -14,49 +14,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // player one (Blue) control for adding
+        // player one (Blue) control for adding health
         p1add.setOnClickListener {
             p1Score++
             player1.text = p1Score.toString()
         }
 
-        // player one (Blue) control for subtracting
+        // player one (Blue) control for subtracting health
         p1sub.setOnClickListener {
             p1Score--
             player1.text = p1Score.toString()
         }
 
-        // player two (White) control for adding
+        // player two (White) control for adding health
         p2add.setOnClickListener {
             p2Score++
             player2.text = p2Score.toString()
         }
 
-        // player two (White) control for subtracting
+        // player two (White) control for subtracting health
         p2sub.setOnClickListener {
             p2Score--
             player2.text = p2Score.toString()
         }
 
-        // player three (Green) control for adding
+        // player three (Green) control for adding health
         p3add.setOnClickListener {
             p3Score++
             player3.text = p3Score.toString()
         }
 
-        // player three (Green) control for subtracting
+        // player three (Green) control for subtracting health
         p3sub.setOnClickListener {
             p3Score--
             player3.text = p3Score.toString()
         }
 
-        // player four (Pink) control for adding
+        // player four (Pink) control for adding health
         p4add.setOnClickListener {
             p4Score++
             player4.text = p4Score.toString()
         }
 
-        // player four (Pink) control for subtracting
+        // player four (Pink) control for subtracting health
         p4sub.setOnClickListener {
             p4Score--
             player4.text = p4Score.toString()
